94fc3ea89bf24a5fc6c3f29d4a43e007b3e39723
Amanda Cole <amandaj3038@gmail.com>
2020-07-27 15:29:10 +1000

    Amanda

---
+++ read_google_sheets_script.R
@@ -0,0 +1,3 @@
+install.packages("googlesheets4")
+library(googlesheets4)
+DASA_MEMBERS<- read_sheet("https://docs.google.com/spreadsheets/d/1iDwWurNO2mcSKRgTEUCTNMbH42JK4s2zjNsdZFD22K8/edit#gid=1722549277")
