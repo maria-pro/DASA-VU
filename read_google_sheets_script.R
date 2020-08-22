c8a9c2e388ec6ee0b3f4fe0b1c2acdf98691a7c4
amandajcole <amandaj3038@gmail.com>
2020-08-22 17:06:29 +1000

    Amanda

--- read_google_sheets_script.R
+++ read_google_sheets_script.R
@@ -1,3 +1,4 @@
 install.packages("googlesheets4")
 library(googlesheets4)
 DASA_MEMBERS<- read_sheet("https://docs.google.com/spreadsheets/d/1iDwWurNO2mcSKRgTEUCTNMbH42JK4s2zjNsdZFD22K8/edit#gid=1722549277")
+DASA_CONSULTATION <-read_sheet("https://docs.google.com/spreadsheets/d/1BBW-RavRXMKuK0OogGdAX_Cdprfv3MQxl_5HvjJU4q4/edit#gid=929382715")
